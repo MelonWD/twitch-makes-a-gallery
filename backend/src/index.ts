bec36be74b20218d34a5c15a4c64905c9fc0b01e
Nathan Dawson <nathan@melonwebdesign.co.uk>
2019-08-15 15:25:36 +0100

    feat: default port | environment port | variable name fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,10 +8,7 @@ import { promises as fs } from 'fs';
 // Internal deps
 import UploadRoute from './upload/upload.route';
 import UploadProcessor from './upload/upload.processor';
-import envionment from './config/environment';
 import environment from './config/environment';
-
-
 
 // Load the configuration into the environment files.
 require('dotenv').config()
@@ -20,26 +17,26 @@ require('dotenv').config()
 // TODO: consider moving these folders within dist
 const imageUploadDirectory = path.join(__dirname, environment.UPLOAD_ROOT)
 
-const temporaryUploadDirectory = path.join(imageUploadDirectory, envionment.TEMPORARY_FOLDER_NAME)
+const temporaryUploadDirectory = path.join(imageUploadDirectory, environment.TEMPORARY_FOLDER_NAME)
 
-const publishedUploadDirectory = path.join(imageUploadDirectory, envionment.PUBLISHED_FOLDER_NAME);
+const publishedUploadDirectory = path.join(imageUploadDirectory, environment.PUBLISHED_FOLDER_NAME);
 
-const archivedFolderName = path.join(imageUploadDirectory, envionment.ARCHIVED_FOLDER_NAME);
-
+const archivedFolderName = path.join(imageUploadDirectory, environment.ARCHIVED_FOLDER_NAME);
 
 // Create a new instance of express
 const app = express();
 
 // Listen to provided port or 3000
-const applicationPort = process.env.ENV || 3000;
+const applicationPort = process.env.PORT || environment.port || 3000;
 
 // Setup upload so we can retrieve uploaded files.
 const upload = multer({ dest: temporaryUploadDirectory })
 
 // Setup the JSON parser so we can retrieve JSON bodies sent.
 app.use(express.json());
-app.use(cors());
 
+// Add cors to each request
+app.use(cors());
 
 // Endpoint for uploading an image
 app.post('/upload', upload.single('file'), UploadRoute.postImage.bind(UploadRoute));
@@ -47,7 +44,7 @@ app.post('/upload', upload.single('file'), UploadRoute.postImage.bind(UploadRout
 // Endpoint for retrieving the currently active image.
 app.get('/image', UploadRoute.getImage.bind(UploadRoute));
 
-// Static route pointing to our published files folder.
+// Static route pointing to our published files folder that 
 app.use('/images', express.static(publishedUploadDirectory));
 
 // Listen on the application port.
@@ -59,7 +56,6 @@ app.listen(applicationPort, () => {
 // Use a simple setTimeout to control the time between image displays
 setInterval(cycleImages, 2000);
 
-
 async function cycleImages() {
     const oldestFile = await UploadProcessor.getOldestFile(publishedUploadDirectory);
 
@@ -69,14 +65,13 @@ async function cycleImages() {
 
     const currentTime = (new Date()).getTime();
 
-    const isOlderThanTimeout = (currentTime - oldestFile.time > 10000);
+    const isOlderThanTimeout = (currentTime - oldestFile.time > 20000);
 
     if (!isOlderThanTimeout) {
         return;
     }
 
     const nextFile = await UploadProcessor.getOldestFile(publishedUploadDirectory, 1);
-
 
     if (!nextFile) {
         return;
